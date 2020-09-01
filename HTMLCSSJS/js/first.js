4fd3da296535d940eca9b6febbbd75be7dba9b2e
golovasadovaya <fynjy-00@inbox.ru>
2020-09-02 00:33:00 +0300

    first

---
+++ HTMLCSSJS/js/first.js
@@ -0,0 +1,38 @@
+var str='Привет, Мир!';
+document.write(str);
+
+var str1='Привет';
+var str2=', Мир!';
+document.write(str1+str2);
+
+name='Anton';
+age=21;
+
+document.write('Привет'+name);
+
+document.write('Мне'+age+'лет!');
+
+var name1=prompt();
+document.write("   Ваше имя"+name1);
+
+var num=prompt();
+document.write(num*num);
+alert(num*num);
+
+var numberq=47;
+numberq +=7;
+document.write(numberq);
+numberq-=18;
+document.write(numberq);
+numberq*=10;
+document.write(numberq);
+numberq/=15;
+document.write(numberq);
+
+var numberw=10;
+document.write(numberw);
+numberw++;
+document.write(numberw);
+numberw--;
+document.write(numberw);
+alert
---
+++ HTMLCSSJS/js/second.js
@@ -0,0 +1,31 @@
+var arr =['a','b','c'];
+
+document.write(arr[0]+arr[1]+arr[2]);
+
+var arr1=['a','b','c','d'];
+
+document.write(arr1[0]+'+'+arr1[1]+','+arr1[2]+'+'+arr1[3]);
+
+var arr2=[2,5,3,9];
+var result=2*5+3*9;
+document.write(result);
+
+var obj= { a:1, b:2, c:3};
+document.write(obj.c);
+document.write(obj['c']);
+
+
+var obj1 = {Коля: '1000',Вася:'500',Петя:'200'};
+
+document.write('  ');
+document.write(obj1.Петя);
+document.write('    ');
+document.write(obj1['Коля']);
+
+document.write('                ');
+var mas= [[1,2,3],[4,5,6],[7,8,9]];
+document.write(mas[1][0]);
+
+document.write('           ');
+var mas1={js:['jQuery','Angular'],php:'hello',css:'world'};
+document.write(mas1['js'][0]);
